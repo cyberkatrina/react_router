679e6d5ec01f4e4a9983ce4b24262e091f78aa09
cyberkatrina <katrinaaliashkevich@utexas.edu>
2024-09-10 23:34:40 -0500

    routes and links implemented

--- src/Router.js
+++ src/Router.js
@@ -7,11 +7,24 @@ to the About component.
 
 Remember to export the component at the end
 */
-
 import React from 'react'
-import { Routes, Route } from 'react-router'
-// Write component imports here //
+import { Routes, Route} from 'react-router-dom';
+
+import About from './components/About';
+import Home from './components/Home';
+import Car from './components/Car';
 
 
+const Router = () => {
+  return (
+    /* Then we use Routes and Route. Routes acts like a regular JS Switch Statement */
+    <Routes>
+        {/* depending on the path in the URL, one of these Routes will be returned and their component rendered */}
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/car/:id" element={<Car/>} />
+    </Routes>
+  );
+}
 
-// Start Router function here //
+export default Router
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import cars from '../cars.json'
-// Import {useParams} from "react-router-dom" here //
+import {useParams} from 'react-router-dom'
 
 // import MUI components here //
-// Container, Paper, Chip //
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Chip from '@mui/material/Chip';
+
 
 const Car = (props) => {
+    const {id} = useParams()
+    const car = cars.find((car) => car.id == id)
+    console.log(car)
+
     return (
-        <h1>A specific car</h1>
+        <div>
+            <Container maxWidth="sm" >
+                <Paper elevation={1} style={{padding: '50px', marginTop: '50px'}}>
+                    <h1>{car.Name}</h1>
+                    {Object.keys(car).map((key, index) => {
+                        return(
+                            <Chip key={index} label={[key+ ': ' + car[key]]}/>
+                        )
+                    })}
+                </Paper>
+            </Container>
+        </div>
     )
 }
 
